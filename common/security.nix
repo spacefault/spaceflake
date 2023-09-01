66c470c7b69a3fccc7b78584ad76270345076814
spacefault <74156492+spacefault@users.noreply.github.com>
2023-09-01 00:02:04 -0600

    security: add swaylock pam settings
    this commit brings the swaylock pam settings so that swaylock can
    function properly
    
    Signed-off-by: spacefault <74156492+spacefault@users.noreply.github.com>

--- common/security.nix
+++ common/security.nix
@@ -6,6 +6,9 @@
     polkit.enable = true;
     pam = {
       services.swaylock = {};
+      services.swaylock.text = ''
+        auth include login
+      '';
       services.gnome-keyring.gnupg.enable = true;
       services.gnome-keyring.sshAgentAuth = true;
       services.gnome-keyring.enableGnomeKeyring = true;
