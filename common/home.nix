38898b8f60b525335ba10a9a211348b2d4597cac
spacefault <74156492+spacefault@users.noreply.github.com>
2023-09-16 11:26:35 -0600

    home: change zsh configuration
    This commit changes the zsh configuration for the system.
    
    Signed-off-by: spacefault <74156492+spacefault@users.noreply.github.com>

--- common/home.nix
+++ common/home.nix
@@ -12,7 +12,6 @@
     };
     zsh = {
       enable = true;
-      enableAutosuggestions = true;
       initExtra = "autoload -Uz vcs_info\nprecmd() { vcs_info }\nzstyle ':vcs_info:git:*' formats '%b '\nsetopt PROMPT_SUBST\nPROMPT='%B%F{green}%n@%m%f%b %B%F{blue}%~%f%b %B%F{red}$vcs_info_msg_0_%f%b> '";
       history = {
         path = "/home/devin/histfile";
