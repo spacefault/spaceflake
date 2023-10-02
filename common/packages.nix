0b9126a54609c67025cf6648d929c17cc1a7c3c2
spacefault <74156492+spacefault@users.noreply.github.com>
2023-10-01 22:27:09 -0600

    Revert "Revert "Revert "services: add kde desktop"""
    This reverts commit 910bdc3f4269f027cc0c50dff9e812f120f9b267.
    
    i'm pretty sure i'm going insane

--- common/packages.nix
+++ common/packages.nix
@@ -74,8 +74,7 @@
     anyrun.packages.${pkgs.system}.anyrun-with-all-plugins
     gnome.gnome-tweaks
     element-desktop
-    libsForQt5.ksshaskpass
-    pinentry
+    remmina
 
     # Programming
     (python3.withPackages(ps: with ps; [ pandas matplotlib requests rich ]))
@@ -91,15 +90,11 @@
       enable = true;
     };
     seahorse = {
-      enable = false;
+      enable = true;
     };
     dconf = {
       enable = true;
     };
-    #ssh = {
-    #  startAgent = true;
-    #  askPassword = pkgs.lib.mkForce "${pkgs.ksshaskpass.out}/bin/ksshaskpass";
-    #};
     gnupg = {
       agent = {
         enable = true;
