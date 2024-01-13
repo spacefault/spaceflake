3732e95775d0c5793779823cda3f0a1d211e403a
spacefault <74156492+spacefault@users.noreply.github.com>
2024-01-13 13:25:46 -0700

    a few changes

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -3,6 +3,13 @@
   pkgs,
   ...
 }: {
+  users.users.lily = {
+    isNormalUser = true;
+    description = "lily";
+    extraGroups = ["networkmanager" "wheel" "audio" "video" "input" "libvirtd" "docker"];
+    packages = with pkgs; [];
+    shell = pkgs.zsh;
+  };
   users.users.devin = {
     isNormalUser = true;
     description = "devin";
