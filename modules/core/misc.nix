e259c50662a4c81c2c9fa6dc3ab9298d4b64e0ba
spacefault <74156492+spacefault@users.noreply.github.com>
2024-01-19 23:22:35 -0700

    misc: remove xdg-desktop-portal warning

--- modules/core/misc.nix
+++ modules/core/misc.nix
@@ -13,9 +13,11 @@
 
   xdg.portal = {
     enable = true;
+    config.common.default = "*";
     extraPortals = with pkgs; [
     ];
   };
+
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   hardware.pulseaudio.enable = false;
 }
