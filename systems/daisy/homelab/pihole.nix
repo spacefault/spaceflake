c3d30bb5ffe7ee7938aed6fc77716bd2c5f0a8cc
spacefault <74156492+spacefault@users.noreply.github.com>
2024-01-25 01:25:55 -0700

    homelab: pihole updates

--- systems/daisy/homelab/pihole.nix
+++ systems/daisy/homelab/pihole.nix
@@ -4,26 +4,30 @@ let
   serverIP = "192.168.1.175";
 in
 {
-  virtualisation.oci-containers.containers.pihole = {
-    image = "pihole/pihole:latest";
-    ports = [
-      "${serverIP}:53:53/tcp"
-      "${serverIP}:53:53/udp"
-      "3080:80"
-      "30443:443"
-    ];
-    volumes = [
-      "/var/lib/pihole/:/etc/pihole/"
-      "/var/lib/dnsmasq.d:/etc/dnsmasq.d/"
-    ];
-    environment = {
-      ServerIP = serverIP;
-      TZ = "Canada/Edmonton";
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers.pihole = {
+      autoStart = true;
+      image = "pihole/pihole:latest";
+      ports = [
+        "${serverIP}:53:53/tcp"
+        "${serverIP}:53:53/udp"
+        "3080:80"
+        "30443:443"
+      ];
+      volumes = [
+        "/var/lib/pihole/:/etc/pihole/"
+        "/var/lib/dnsmasq.d:/etc/dnsmasq.d/"
+      ];
+      environment = {
+        ServerIP = serverIP;
+        TZ = "Canada/Edmonton";
+      };
+      extraOptions = [
+        "--cap-add=NET_ADMIN"
+        "--dns=127.0.0.1"
+        "--dns=1.1.1.1"
+      ];
     };
-    extraOptions = [
-      "--cap-add=NET_ADMIN"
-      "--dns=127.0.0.1"
-      "--dns=1.1.1.1"
-    ];
   };
 }
