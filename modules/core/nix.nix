9d0459e3fb650d9522f4adeece9581c70484825c
spacefault <74156492+spacefault@users.noreply.github.com>
2024-01-29 22:22:11 -0700

    nix: add cachix for nix-gaming

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -4,6 +4,8 @@
       experimental-features = ["nix-command" "flakes"];
       allowed-users = ["@wheel" "devin"];
       auto-optimise-store = true;
+      substituters = ["https://nix-gaming.cachix.org"];
+      trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
     };
     gc = {
       automatic = true;
