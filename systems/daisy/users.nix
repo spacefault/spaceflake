0a338529b61d258dbac010b4f5b3adb56749c6ae
spacefault <74156492+spacefault@users.noreply.github.com>
2024-01-30 15:13:50 -0700

    daisy: add ssh key

--- systems/daisy/users.nix
+++ systems/daisy/users.nix
@@ -14,6 +14,7 @@
     # ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC05s+wxlFUMx9hjB9LeZLNhvzuC1khMNJgBQ2F5HY/w lily@blueberry"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAMcQFmwgMoiID6kaJSWQCbMKiGRvF5v0A5athSGruaD lily@cherry"
     ];
   };
   users = {
