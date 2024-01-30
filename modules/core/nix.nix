c8848de66832ef0def77befc8714bb202597182c
spacefault <74156492+spacefault@users.noreply.github.com>
2024-01-29 22:23:05 -0700

    nix: remove legacy things

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -2,7 +2,7 @@
   nix = {
     settings = {
       experimental-features = ["nix-command" "flakes"];
-      allowed-users = ["@wheel" "devin"];
+      allowed-users = ["@wheel"];
       auto-optimise-store = true;
       substituters = ["https://nix-gaming.cachix.org"];
       trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
