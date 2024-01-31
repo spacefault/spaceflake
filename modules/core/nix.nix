10e1bb11cf17393d521586fd078bdd95e909d06e
spacefault <74156492+spacefault@users.noreply.github.com>
2024-01-31 08:52:41 -0700

    nix: add myself as a trusted user

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -2,7 +2,7 @@
   nix = {
     settings = {
       experimental-features = ["nix-command" "flakes"];
-      allowed-users = ["@wheel"];
+      allowed-users = ["@wheel" "lily"];
       auto-optimise-store = true;
       substituters = ["https://nix-gaming.cachix.org"];
       trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
