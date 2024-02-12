77fc663065bd98c9d1d95203f5a5f80dcb09a7e6
spacefault <74156492+spacefault@users.noreply.github.com>
2024-02-12 01:41:00 -0700

    some changes

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -8,7 +8,7 @@
     enable = true;
     settings = {
       window = {
-        opacity = 0.8;
+        opacity = 0.9;
       };
       shell = {
         program = "/run/current-system/sw/bin/zsh";
