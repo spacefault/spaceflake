867ddf2dc5ec7a7996c0f206490029fc31456602
spacefault <74156492+spacefault@users.noreply.github.com>
2024-02-14 12:49:52 -0700

    home/hyprland: fix specialworkspace bind

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -146,7 +146,7 @@ in {
           "${mod}, up, movefocus, u"
           "${mod}, down, movefocus, d"
           "${mod}, grave, togglespecialworkspace"
-          "SUPERSHIFT, grade, movetoworkspace, special"
+          "SUPERSHIFT, grave, movetoworkspace, special"
           ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
           ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
           ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
