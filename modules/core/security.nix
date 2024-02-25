5463c4ca65692007698373decbfd1510393b3abe
Lily <74156492+spacefault@users.noreply.github.com>
2024-02-25 00:22:05 -0700

    Update security.nix

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -1,4 +1,4 @@
-{...}: {
+{config, lib, pkgs, ...}: {
   security = {
     rtkit.enable = true;
     polkit.enable = true;
@@ -12,4 +12,159 @@
       services.gnome-keyring.enableGnomeKeyring = true;
     };
   };
-}
+
+# the following is merged from
+# https://github.com/hlissner/dotfiles/blob/master/modules/security.nix
+# https://github.com/sioodmy/dotfiles/blob/main/system/core/schizo.nix
+
+### security modifications
+
+  ## System security tweaks
+  # sets hidepid=2 on /proc (make process info visible only to owning user)
+  # NOTE Was removed on nixpkgs-unstable because it doesn't do anything
+  # security.hideProcessInformation = true;
+  # Prevent replacing the running kernel w/o reboot
+  security.protectKernelImage = true;
+
+  # tmpfs = /tmp is mounted in ram. Doing so makes temp file management speedy
+  # on ssd systems, and volatile! Because it's wiped on reboot.
+  boot.tmpOnTmpfs = lib.mkDefault true;
+  # If not using tmpfs, which is naturally purged on reboot, we must clean it
+  # /tmp ourselves. /tmp should be volatile storage!
+  boot.cleanTmpDir = lib.mkDefault (!config.boot.tmpOnTmpfs);
+
+  # Fix a security hole in place for backwards compatibility. See desc in
+  # nixpkgs/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix
+  boot.loader.systemd-boot.editor = false;
+
+  boot.kernel.sysctl = {
+    # The Magic SysRq key is a key combo that allows users connected to the
+    # system console of a Linux kernel to perform some low-level commands.
+    # Disable it, since we don't need it, and is a potential security concern.
+    "kernel.sysrq" = 0;
+
+    ## TCP hardening
+    # Prevent bogus ICMP errors from filling up logs.
+    "net.ipv4.icmp_ignore_bogus_error_responses" = 1;
+    # Reverse path filtering causes the kernel to do source validation of
+    # packets received from all interfaces. This can mitigate IP spoofing.
+    "net.ipv4.conf.default.rp_filter" = 1;
+    "net.ipv4.conf.all.rp_filter" = 1;
+    # Do not accept IP source route packets (we're not a router)
+    "net.ipv4.conf.all.accept_source_route" = 0;
+    "net.ipv6.conf.all.accept_source_route" = 0;
+    # Don't send ICMP redirects (again, we're on a router)
+    "net.ipv4.conf.all.send_redirects" = 0;
+    "net.ipv4.conf.default.send_redirects" = 0;
+    # Refuse ICMP redirects (MITM mitigations)
+    "net.ipv4.conf.all.accept_redirects" = 0;
+    "net.ipv4.conf.default.accept_redirects" = 0;
+    "net.ipv4.conf.all.secure_redirects" = 0;
+    "net.ipv4.conf.default.secure_redirects" = 0;
+    "net.ipv6.conf.all.accept_redirects" = 0;
+    "net.ipv6.conf.default.accept_redirects" = 0;
+    # Protects against SYN flood attacks
+    "net.ipv4.tcp_syncookies" = 1;
+    # Incomplete protection again TIME-WAIT assassination
+    "net.ipv4.tcp_rfc1337" = 1;
+
+    ## TCP optimization
+    # TCP Fast Open is a TCP extension that reduces network latency by packing
+    # data in the sender’s initial TCP SYN. Setting 3 = enable TCP Fast Open for
+    # both incoming and outgoing connections:
+    "net.ipv4.tcp_fastopen" = 3;
+    # Bufferbloat mitigations + slight improvement in throughput & latency
+    "net.ipv4.tcp_congestion_control" = "bbr";
+    "net.core.default_qdisc" = "cake";
+  };
+  boot.kernelModules = [ "tcp_bbr" ];
+  apparmor = {
+      enable = true;
+      killUnconfinedConfinables = true;
+      packages = [pkgs.apparmor-profiles];
+     };
+    boot.kernel.sysctl = {
+    # Hide kernel pointers from processes without the CAP_SYSLOG capability.
+    "kernel.kptr_restrict" = 1;
+    "kernel.printk" = "3 3 3 3";
+    # Restrict loading TTY line disciplines to the CAP_SYS_MODULE capability.
+    "dev.tty.ldisc_autoload" = 0;
+    # Make it so a user can only use the secure attention key which is required to access root securely.
+    "kernel.sysrq" = 4;
+    # Protect against SYN flooding.
+    "net.ipv4.tcp_syncookies" = 1;
+    # Protect against time-wait assasination.
+    "net.ipv4.tcp_rfc1337" = 1;
+
+    # Enable strict reverse path filtering (that is, do not attempt to route
+    # packets that "obviously" do not belong to the iface's network; dropped
+    # packets are logged as martians).
+    "net.ipv4.conf.all.log_martians" = true;
+    "net.ipv4.conf.all.rp_filter" = "1";
+    "net.ipv4.conf.default.log_martians" = true;
+    "net.ipv4.conf.default.rp_filter" = "1";
+
+    # Protect against SMURF attacks and clock fingerprinting via ICMP timestamping.
+    "net.ipv4.icmp_echo_ignore_all" = "1";
+
+    # Ignore incoming ICMP redirects (note: default is needed to ensure that the
+    # setting is applied to interfaces added after the sysctls are set)
+    "net.ipv4.conf.all.accept_redirects" = false;
+    "net.ipv4.conf.all.secure_redirects" = false;
+    "net.ipv4.conf.default.accept_redirects" = false;
+    "net.ipv4.conf.default.secure_redirects" = false;
+    "net.ipv6.conf.all.accept_redirects" = false;
+    "net.ipv6.conf.default.accept_redirects" = false;
+
+    # Ignore outgoing ICMP redirects (this is ipv4 only)
+    "net.ipv4.conf.all.send_redirects" = false;
+    "net.ipv4.conf.default.send_redirects" = false;
+
+    # Restrict abritrary use of ptrace to the CAP_SYS_PTRACE capability.
+    "kernel.yama.ptrace_scope" = 2;
+    "net.core.bpf_jit_enable" = false;
+    "kernel.ftrace_enabled" = false;
+  };
+
+  # Security
+  boot.blacklistedKernelModules = [
+    # Obscure network protocols
+    "ax25"
+    "netrom"
+    "rose"
+    # Old or rare or insufficiently audited filesystems
+    "adfs"
+    "affs"
+    "bfs"
+    "befs"
+    "cramfs"
+    "efs"
+    "erofs"
+    "exofs"
+    "freevxfs"
+    "f2fs"
+    "vivid"
+    "gfs2"
+    "ksmbd"
+    "nfsv4"
+    "nfsv3"
+    "cifs"
+    "nfs"
+    "cramfs"
+    "freevxfs"
+    "jffs2"
+    "hfs"
+    "hfsplus"
+    "squashfs"
+    "udf"
+    "hpfs"
+    "jfs"
+    "minix"
+    "nilfs2"
+    "omfs"
+    "qnx4"
+    "qnx6"
+    "sysv"
+  ];  
+  
+  }
