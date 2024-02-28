0ab903b79f736dd957d4885af3d9eb124e8d6512
spacefault <74156492+spacefault@users.noreply.github.com>
2024-02-28 16:44:59 -0700

    move to cachix
    and a bit of cleanup

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -8,10 +8,14 @@
         "https://cache.nixos.org?priority=10"
         "https://nix-community.cachix.org"
         "https://nix-gaming.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://anyrun.cachix.org"
       ];
       trusted-public-keys = [
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
       ];
     };
     gc = {
