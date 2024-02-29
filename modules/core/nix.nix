7cf3e5632d09f378a1212d9f0b4879ec9f99fcf3
spacefault <74156492+spacefault@users.noreply.github.com>
2024-02-29 01:12:13 -0700

    change nix settings

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -4,6 +4,7 @@
       experimental-features = ["nix-command" "flakes"];
       allowed-users = ["@wheel" "lily"];
       auto-optimise-store = true;
+      builders-use-substitutes = true;
       substituters = [
         "https://cache.nixos.org?priority=10"
         "https://nix-community.cachix.org"
