e73dc42829b9956e2b7d3c787acd4fc3ae9c9f05
spacefault <74156492+spacefault@users.noreply.github.com>
2024-03-08 02:05:30 -0700

    a bunch of changes

--- home/home.nix
+++ home/home.nix
@@ -17,20 +17,20 @@
 
   imports = [
     inputs.anyrun.homeManagerModules.default
-    ./waybar.nix
+    #./waybar.nix
     ./zsh.nix
     ./emacs.nix
     ./neovim.nix
     ./alacritty.nix
-    ./dunst.nix
+    #./dunst.nix
     ./swaylock.nix
     #./swayidle.nix
-    ./hyprland.nix
+    #./hyprland.nix
     ./spicetify.nix
-    ./anyrun.nix
+    #./anyrun.nix
     ./vscode.nix
-    ./gtk.nix
+    #./gtk.nix
     ./tmux.nix
-    ./swayosd.nix
+    #./swayosd.nix
   ];
 }
