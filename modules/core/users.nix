7d01838db203249020521eaa7593201f3834fe35
spacefault <74156492+spacefault@users.noreply.github.com>
2024-03-09 16:10:34 -0700

    Revert "testing stuff"
    This reverts commit 90dd009d1a19102dbd0f8fdaee34ea1f99895de8.

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -1,8 +1,6 @@
 {pkgs, ...}: {
-  users.mutableUsers = false;
   users.users.lily = {
     isNormalUser = true;
-    hashedPasswordFile = "/persist/passwords/lily/lily";
     description = "lily";
     extraGroups = ["networkmanager" "wheel" "audio" "video" "input" "libvirtd" "docker"];
     packages = with pkgs; [];
