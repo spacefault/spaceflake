90dd009d1a19102dbd0f8fdaee34ea1f99895de8
spacefault <74156492+spacefault@users.noreply.github.com>
2024-03-09 15:54:53 -0700

    testing stuff

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -1,6 +1,8 @@
 {pkgs, ...}: {
+  users.mutableUsers = false;
   users.users.lily = {
     isNormalUser = true;
+    hashedPasswordFile = "/persist/passwords/lily/lily";
     description = "lily";
     extraGroups = ["networkmanager" "wheel" "audio" "video" "input" "libvirtd" "docker"];
     packages = with pkgs; [];
