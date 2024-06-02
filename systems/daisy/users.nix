b3fac27cf8c7ec2216a874a364ffabd230aeb48a
spacefault <74156492+spacefault@users.noreply.github.com>
2024-06-02 17:40:57 -0600

    changes

--- systems/daisy/users.nix
+++ systems/daisy/users.nix
@@ -9,8 +9,7 @@
     #   /etc/nixos/ssh/authorized_keys
     # ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC05s+wxlFUMx9hjB9LeZLNhvzuC1khMNJgBQ2F5HY/w lily@blueberry"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAMcQFmwgMoiID6kaJSWQCbMKiGRvF5v0A5athSGruaD lily@cherry"
+      ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKhnw4C62bwoG9Xon0cHgoa0hJi1nLzVUsqqydJBtZ0s lily@cherry
     ];
   };
   users = {
