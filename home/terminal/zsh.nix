660a23f30c6ff88d8f45f5f073004e910443185e
spacefault <74156492+spacefault@users.noreply.github.com>
2024-10-27 15:36:08 -0600

    changes

--- home/terminal/zsh.nix
+++ home/terminal/zsh.nix
@@ -2,7 +2,7 @@
   programs = {
     zsh = {
       enable = true;
-      initExtra = "DISABLE_AUTO_TITLE=false\nautoload -Uz vcs_info\nprecmd() { vcs_info }\nzstyle ':vcs_info:git:*' formats '%b '\nsetopt PROMPT_SUBST\nPROMPT='%B%F{243}%n %bon %B%m%f%b %B%F{197}%~%f%b %B%F{39}$vcs_info_msg_0_%f%b> '";
+      initExtra = "DISABLE_AUTO_TITLE=false\nautoload -Uz vcs_info\nprecmd() { vcs_info }\nzstyle ':vcs_info:git:*' formats '%b '\nsetopt PROMPT_SUBST\nPROMPT='%B%F{243}%n %bon %B%m%f%b %B%F{197}%2~%f%b %B%F{39}$vcs_info_msg_0_%f%b> '";
       history = {
         path = "/home/lily/.config/zsh/histfile";
       };
