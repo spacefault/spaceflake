7cc5aa9297d9fc0a6248a2e7282c886909705320
spacefault <74156492+spacefault@users.noreply.github.com>
2024-12-01 17:20:14 -0700

    change laptop swap device

--- profiles/hardware-laptop.nix
+++ profiles/hardware-laptop.nix
@@ -30,7 +30,12 @@
     options = ["fmask=0077" "dmask=0077"];
   };
 
-  swapDevices = [];
+  swapDevices = [
+    { 
+      device = "/dev/disk/by-partuuid/7f1cf807-c96c-439b-a28f-2fe7151c5677";
+      randomEncryption.enable = true;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
