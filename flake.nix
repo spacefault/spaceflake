0742faa2da31b96866fffab9c9f0a1c0b371b938
spacefault <74156492+spacefault@users.noreply.github.com>
2024-12-14 13:53:45 -0700

    ahhhhh

--- flake.nix
+++ flake.nix
@@ -21,12 +21,17 @@
       url = "github:kirottu/watershot";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     home-manager,
+    fenix,
     ...
   } @ inputs: let
     inherit (self) outputs;
