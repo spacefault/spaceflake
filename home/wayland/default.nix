87d20e328f54de1cc70f9bfd4efc72678e5f45c8
spacefault <74156492+spacefault@users.noreply.github.com>
2024-12-27 12:12:44 -0700

    changes

--- home/wayland/default.nix
+++ home/wayland/default.nix
@@ -1,15 +1,15 @@
 {...}: {
   imports = [
-    #./anyrun.nix
-    #./dunst.nix
-    #./gtk.nix
-    #./hyprland.nix
+    ./anyrun.nix
+    ./dunst.nix
+    ./gtk.nix
+    ./hyprland.nix
     ./monitorLogic.nix
-    #./dependencies.nix
-    #./swayosd.nix
+    ./dependencies.nix
+    ./swayosd.nix
     ./packages.nix
-    #./xdg.nix
-    #./waybar.nix
-    #./polkit.nix
+    ./xdg.nix
+    ./waybar.nix
+    ./polkit.nix
   ];
 }
