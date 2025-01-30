18fc74f88055f56df529c96f94b37420e2ac7618
spacefault <74156492+spacefault@users.noreply.github.com>
2025-01-30 16:49:07 -0700

    change nix conf

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -172,6 +172,7 @@
       use-xdg-base-directories = true;
       substituters = [
         "https://cache.nixos.org?priority=10"
+        
         "https://nix-community.cachix.org"
         "https://nix-gaming.cachix.org"
         "https://hyprland.cachix.org"
@@ -179,6 +180,7 @@
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
