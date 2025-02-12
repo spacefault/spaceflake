3a3b7126259a55f885fef0675e4f0bf7a968924e
spacefault <74156492+spacefault@users.noreply.github.com>
2025-02-12 15:55:09 -0700

    update desktop disks

--- profiles/blueberry.nix
+++ profiles/blueberry.nix
@@ -21,33 +21,38 @@
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/14ffd462-d5ec-4275-a9cb-67f3627af2aa";
+      device = "/dev/disk/by-uuid/0c6811df-46b7-402a-8a54-4f2bf5aed295";
       fsType = "ext4";
     };
 
     "/boot" = {
-      device = "/dev/disk/by-uuid/3A1B-FEA4";
+      device = "/dev/disk/by-uuid/5EFC-C50A";
       fsType = "vfat";
       options = ["fmask=0077" "dmask=0077"];
     };
 
-    #    "/mnt/data" = {
-    #      device = "/dev/disk/by-uuid/569DBC67112060ED";
-    #      fsType = "ntfs3";
-    #      options = ["windows_names"];
-    #    };
-    #
-    #    "/mnt/games" = {
-    #      device = "/dev/disk/by-uuid/38D0D91E037C5F55";
-    #      fsType = "ntfs3";
-    #      options = ["windows_names"];
-    #    };
+    "/mnt/data" = {
+      device = "/dev/disk/by-uuid/C8C41EA7C41E9830";
+      fsType = "ntfs3";
+      options = ["windows_names"];
+    };
+
+    "/mnt/games" = {
+      device = "/dev/disk/by-uuid/C41C138F1C137C1C";
+      fsType = "ntfs3";
+      options = ["windows_names"];
+    };
   };
 
-  swapDevices = [];
+  swapDevices = [
+    { 
+      device = "/dev/disk/by-partuuid/75f9364b-04cc-4708-b795-2e0e83c8943a";
+      randomEncryption.enable = true;
+    }
+  ];
 
   boot = {
-    initrd.luks.devices."luks-941f69c6-eb39-4420-b0e6-79f88f721069".device = "/dev/disk/by-uuid/941f69c6-eb39-4420-b0e6-79f88f721069";
+    initrd.luks.devices."luks-42c4c29b-959b-442f-a862-5d646d3a1fc7".device = "/dev/disk/by-uuid/42c4c29b-959b-442f-a862-5d646d3a1fc7";
     initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
     kernelModules = ["kvm-intel"];
   };
