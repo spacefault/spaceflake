44fa1ea692d4e4816a67f973819bfd0c18b6c5f0
spacefault <74156492+spacefault@users.noreply.github.com>
2025-02-14 15:22:38 +0000

    Cleanup Code

--- profiles/cherry.nix
+++ profiles/cherry.nix
@@ -67,10 +67,10 @@
         CPU_MIN_PERF_ON_BAT = 0;
         CPU_MAX_PERF_ON_BAT = 20;
 
-       #Optional helps save long term battery health
-       START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-       STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
-     };
+        #Optional helps save long term battery health
+        START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      };
     };
   };
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
