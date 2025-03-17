40addba680857953af23b400c022f5ec3000cb73
spacefault <74156492+spacefault@users.noreply.github.com>
2025-03-17 08:49:26 -0600

    refresh laptop conf
    why is my laptop actually so unstable bruh

--- home/terminal/git.nix
+++ home/terminal/git.nix
@@ -4,7 +4,7 @@
     userName = "spacefault";
     userEmail = "74156492+spacefault@users.noreply.github.com";
     signing = {
-      key = "68B88586B574840C";
+      key = "2F9AE33488378FEB";
       signByDefault = true;
     };
   };
