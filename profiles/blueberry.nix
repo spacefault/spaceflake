b152c4eb96902aa8286f9649afcc7bf7f53f5cdf
spacefault <74156492+spacefault@users.noreply.github.com>
2025-03-31 10:25:22 -0600

    sync laptop

--- profiles/blueberry.nix
+++ profiles/blueberry.nix
@@ -13,8 +13,6 @@
     bluetooth.powerOnBoot = true;
   };
 
-  environment.sessionVariables.SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
-
   imports = [
     ../modules/desktop.nix
     ../modules/gaming.nix
