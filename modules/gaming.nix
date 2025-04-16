5e0ca29eb2cea1f23b6e7621668f39164e887eeb
spacefault <74156492+spacefault@users.noreply.github.com>
2025-04-15 22:33:27 -0600

    steam updates

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -30,35 +30,35 @@
       enable = true;
       gamescopeSession.enable = true;
       extraCompatPackages = [pkgs.proton-ge-bin];
-      package = pkgs.steam.override {
-        extraBwrapArgs = [
-          "--bind $HOME/tmp/steamhome $HOME"
-          "--unsetenv XDG_CACHE_HOME"
-          "--unsetenv XDG_CONFIG_HOME"
-          "--unsetenv XDG_DATA_HOME"
-          "--unsetenv XDG_STATE_HOME"
-        ];
-        extraPkgs = pkgs:
-          with pkgs; [
-            pango
-            gamemode
-            libthai
-            harfbuzz
-            keyutils
-            libkrb5
-            libpng
-            libpulseaudio
-            libvorbis
-            stdenv.cc.cc.lib
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXinerama
-            xorg.libXScrnSaver
-          ];
-      };
     };
-    gamemode = {
-      enable = true;
+    gamemode.enable = true;
+  };
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraBwrapArgs = [
+        "--bind $HOME/etc/steamhome $HOME"
+        "--unsetenv XDG_CACHE_HOME"
+        "--unsetenv XDG_CONFIG_HOME"
+        "--unsetenv XDG_DATA_HOME"
+        "--unsetenv XDG_STATE_HOME"
+      ];
+      extraPkgs = pkgs:
+        with pkgs; [
+          pango
+          gamemode
+          libthai
+          harfbuzz
+          keyutils
+          libkrb5
+          libpng
+          libpulseaudio
+          libvorbis
+          stdenv.cc.cc.lib
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
+        ];
     };
   };
 }
