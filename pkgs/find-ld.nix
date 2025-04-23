24f3afad05d1a73ff51d024e8370f5baa0f495c3
spacefault <74156492+spacefault@users.noreply.github.com>
2025-04-23 06:02:35 +0000

    Cleanup Code

--- pkgs/find-ld.nix
+++ pkgs/find-ld.nix
@@ -1,38 +1,36 @@
-{ pkgs }:
-
+{pkgs}:
 # simple tool to find the needed libraries of a binary
 # example: find-ld /path/to/binary.out
-
 pkgs.writeShellScriptBin "find-ld" ''
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <filename>"
-    exit 1
-fi
+  if [ $# -ne 1 ]; then
+      echo "Usage: $0 <filename>"
+      exit 1
+  fi
 
-filename=$1
+  filename=$1
 
-echo "Required Shared Libraries for binary $filename"
-echo
+  echo "Required Shared Libraries for binary $filename"
+  echo
 
-${pkgs.binutils}/bin/readelf -d "$filename" | ${pkgs.gawk}/bin/awk '
-/Type[[:space:]]+Name\/Value/ {
-    sub(/^[[:space:]]+/, "")
-    sub(/^[^[:space:]]+[[:space:]]+/, "")
-    print
-    start=1
-    next
-}
+  ${pkgs.binutils}/bin/readelf -d "$filename" | ${pkgs.gawk}/bin/awk '
+  /Type[[:space:]]+Name\/Value/ {
+      sub(/^[[:space:]]+/, "")
+      sub(/^[^[:space:]]+[[:space:]]+/, "")
+      print
+      start=1
+      next
+  }
 
-start == 1 {
-    sub(/^[[:space:]]+/, "")
+  start == 1 {
+      sub(/^[[:space:]]+/, "")
 
-    sub(/^[^[:space:]]+[[:space:]]+/, "")
+      sub(/^[^[:space:]]+[[:space:]]+/, "")
 
-    if ($0 ~ /RUNPATH/ || $0 ~ /RPATH/ || $0 ~ /NEEDED/) {
-        print
-    }
-}
-  '
+      if ($0 ~ /RUNPATH/ || $0 ~ /RPATH/ || $0 ~ /NEEDED/) {
+          print
+      }
+  }
+    '
 
 ''
