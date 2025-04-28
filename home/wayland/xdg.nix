a9b799f493ad9323ad061942b80c57f2076e8b01
spacefault <74156492+spacefault@users.noreply.github.com>
2025-04-28 18:51:04 +0000

    Cleanup Code

--- home/wayland/xdg.nix
+++ home/wayland/xdg.nix
@@ -1,19 +1,15 @@
-{
-  pkgs,
-  config,
-  ...
-}: {
+{config, ...}: {
   xdg = {
     enable = true;
     cacheHome = config.home.homeDirectory + "/.cache";
     configHome = config.home.homeDirectory + "/.config";
     stateHome = config.home.homeDirectory + "/.local/state";
     dataHome = config.home.homeDirectory + "/.local/share";
-   # portal = {
-   #   enable = true;
-   #   extraPortals = [pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk];
-   #   config.common.default = "*";
-   # };
+    # portal = {
+    #   enable = true;
+    #   extraPortals = [pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk];
+    #   config.common.default = "*";
+    # };
     userDirs = {
       enable = true;
       publicShare = null;
