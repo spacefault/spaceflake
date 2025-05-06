d1a1ae7d1ec20c039472e4fcf1995611305dcba6
spacefault <74156492+spacefault@users.noreply.github.com>
2025-05-06 20:54:33 +0000

    Cleanup Code

--- home/wayland/hypridle.nix
+++ home/wayland/hypridle.nix
@@ -1,45 +1,42 @@
-{pkgs, ...}:
-
-{
+{...}: {
   services.hypridle = {
     enable = true;
     settings = {
       general = {
         lock_cmd = "pidof hyprlock || hyprlock";
         before_sleep_cmd = "loginctl lock-session";
-        after_sleep_cmd ="hyprctl dispatch dpms on";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
       };
 
       listener = [
         {
-        timeout = 150;
-        on-timeout = "brightnessctl -s set 10";
-        on-resume = "brightnessctl -r";
+          timeout = 150;
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
         }
 
-       {
-        timeout = 150;
-        on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
-        on-resume = "brightnessctl -rd rgb:kbd_backlight";
-      }
+        {
+          timeout = 150;
+          on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
+          on-resume = "brightnessctl -rd rgb:kbd_backlight";
+        }
 
-      {
-        timeout = 200;
-        on-timeout = "loginctl lock-session";
-      }
+        {
+          timeout = 200;
+          on-timeout = "loginctl lock-session";
+        }
 
+        {
+          timeout = 330;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
+        }
 
-      {
-        timeout = 330;
-        on-timeout = "hyprctl dispatch dpms off";
-        on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
-      }
-
-      {
-        timeout = 250;
-        on-timeout = "systemctl suspend";
-      }
-    ];
+        {
+          timeout = 250;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
   };
-};
 }
