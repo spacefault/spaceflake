505949604f86654ebac794d25410a66d01598a69
spacefault <74156492+spacefault@users.noreply.github.com>
2025-08-27 21:17:36 -0600

    formatting

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,5 @@
 # pkgs/default.nix
-
-{ pkgs }:
-
-{
-  find-ld = pkgs.callPackage ./find-ld { }; 
-  helvetica-neue = pkgs.callPackage ./helvetica-neue { }; 
+{pkgs}: {
+  find-ld = pkgs.callPackage ./find-ld {};
+  helvetica-neue = pkgs.callPackage ./helvetica-neue {};
 }
