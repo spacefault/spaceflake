4d886a0208d18f5fd898d71223ece21a3b5e0277
spacefault <74156492+spacefault@users.noreply.github.com>
2025-09-08 11:28:05 -0600

    remove nix-gaming

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -14,10 +14,6 @@
     discord
     mangohud
     obs-studio
-    inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin
-    #inputs.nix-gaming.packages.${pkgs.system}.osu-stable
-    #inputs.nix-gaming.packages.${pkgs.system}.wine-discord-ipc-bridge
-    #inputs.nix-gaming.packages.${pkgs.system}.wine-osu
   ];
 
   #systemd = {
