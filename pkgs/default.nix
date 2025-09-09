2348c248b28835b046d569b6db56fb4d8fe69bc8
spacefault <74156492+spacefault@users.noreply.github.com>
2025-09-08 22:07:22 -0600

    update packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,4 +3,6 @@
   find-ld = pkgs.callPackage ./find-ld {};
   helvetica-neue = pkgs.callPackage ./helvetica-neue {};
   sfpro = pkgs.callPackage ./sfpro {};
+  mactahoe-icons = pkgs.callPackage ./mactahoe-icons {};
+  mactahoe-theme = pkgs.callPackage ./mactahoe-theme {};
 }
