09f9df51aa4c5b6c3f422bb09ea3b38d7be96c3e
spacefault <74156492+spacefault@users.noreply.github.com>
2025-09-14 23:59:10 -0600

    add tmux to ghostty

--- home/terminal/ghostty.nix
+++ home/terminal/ghostty.nix
@@ -1,12 +1,30 @@
-{...}: {
+{ pkgs, ... }:
+
+let
+  ghosttyTmuxScript = pkgs.writeShellScript "ghostty-tmux.sh" ''
+    #!/bin/bash
+    SESSION_NAME="ghostty"
+
+    if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+      tmux attach-session -t "$SESSION_NAME"
+    else
+      tmux new-session -s "$SESSION_NAME" -d
+      tmux attach-session -t "$SESSION_NAME"
+    fi
+  '';
+in {
   programs.ghostty = {
-    package = null;
     enable = true;
+    package = null;
+
     settings = {
       macos-titlebar-style = "native";
       macos-titlebar-proxy-icon = "hidden";
       macos-option-as-alt = "true";
       theme = "Adventure";
+
+      command = "${pkgs.bash}/bin/bash -c '${ghosttyTmuxScript}'";
     };
   };
 }
+
