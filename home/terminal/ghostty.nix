fb1632b5c7d45c79c1a4f5895e56dd249b0c3082
spacefault <74156492+spacefault@users.noreply.github.com>
2025-09-18 08:23:54 +0000

    Cleanup Code

--- home/terminal/ghostty.nix
+++ home/terminal/ghostty.nix
@@ -1,14 +1,14 @@
 {pkgs, ...}: let
   ghosttyTmuxScript = pkgs.writeShellScript "ghostty-tmux.sh" ''
-SESSION_NAME="ghostty"
+    SESSION_NAME="ghostty"
 
-if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-  WINDOW_ID=$(tmux new-window -t "$SESSION_NAME" -P -F "#{window_id}")
-  tmux attach-session -t "$SESSION_NAME" \; select-window -t "$WINDOW_ID"
-else
-  tmux new-session -s "$SESSION_NAME"
-fi
- 
+    if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
+      WINDOW_ID=$(tmux new-window -t "$SESSION_NAME" -P -F "#{window_id}")
+      tmux attach-session -t "$SESSION_NAME" \; select-window -t "$WINDOW_ID"
+    else
+      tmux new-session -s "$SESSION_NAME"
+    fi
+
   '';
 in {
   programs.ghostty = {
