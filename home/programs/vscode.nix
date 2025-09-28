480ed8e71099c11d3a5cb7a5e75ce38779dfa77e
spacefault <74156492+spacefault@users.noreply.github.com>
2025-09-28 16:25:40 -0600

    update vscode conf

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -4,6 +4,11 @@
     profiles.default.userSettings = {
       "window.titleBarStyle" = "custom";
       "workbench.sideBar.location" = "right";
+      "github.copilot.enable" = {
+        "*" = "false";
+        "markdown" = "false";
+        "scminput" = "false";
+      };
     };
   };
 }
