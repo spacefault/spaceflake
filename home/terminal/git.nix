ee81388982335459cab8bbd39369e04c9ebcfee9
spacefault <74156492+spacefault@users.noreply.github.com>
2025-10-07 18:41:21 -0600

    update git

--- home/terminal/git.nix
+++ home/terminal/git.nix
@@ -12,6 +12,7 @@
       br = "branch";
       ci = "commit";
       st = "status";
+      mg = "merge";
       lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %aN%C(reset)%C(auto)%d%C(reset)' --all";
     };
     extraConfig = {
