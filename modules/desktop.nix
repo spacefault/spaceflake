fa121734bf112c7e76dc1ed6d8aff0fbcdd067b6
spacefault <74156492+spacefault@users.noreply.github.com>
2025-11-05 00:00:07 +0000

    Cleanup Code

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -310,7 +310,7 @@
       enable32Bit = true;
       extraPackages = with pkgs; [
         libva-vdpau-driver
-	libvdpau-va-gl
+        libvdpau-va-gl
         vulkan-validation-layers
         intel-media-driver
       ];
