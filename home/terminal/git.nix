b4380f32696480d983d86b57dc1b5ba498e64de3
spacefault <74156492+spacefault@users.noreply.github.com>
2025-11-10 02:00:14 -0700

    fix trace warnings

--- home/terminal/git.nix
+++ home/terminal/git.nix
@@ -1,21 +1,24 @@
 {...}: {
   programs.git = {
     enable = true;
-    userName = "spacefault";
-    userEmail = "74156492+spacefault@users.noreply.github.com";
-    signing = {
-      key = "85AABA1FC9EC2A0A";
-      signByDefault = true;
-    };
-    aliases = {
-      co = "checkout";
-      br = "branch";
-      ci = "commit";
-      st = "status";
-      mg = "merge";
-      lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %aN%C(reset)%C(auto)%d%C(reset)' --all";
-    };
-    extraConfig = {
+    settings = {
+      user = {
+        name = "spacefault";
+        email = "74156492+spacefault@users.noreply.github.com";
+      };
+      signing = {
+        key = "85AABA1FC9EC2A0A";
+        signByDefault = true;
+      };
+      alias = {
+        co = "checkout";
+        br = "branch";
+        ci = "commit";
+        st = "status";
+        mg = "merge";
+        lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %aN%C(reset)%C(auto)%d%C(reset)' --all";
+      };
+
       core.editor = "nvim";
       pull.rebase = true;
       init.defaultBranch = "master";
