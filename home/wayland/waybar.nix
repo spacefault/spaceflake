e224524fc7df8dc71b603c028e2357d9872278a3
spacefault <74156492+spacefault@users.noreply.github.com>
2025-11-27 12:17:18 -0700

    update waybar

--- home/wayland/waybar.nix
+++ home/wayland/waybar.nix
@@ -6,7 +6,7 @@
       layer = "top";
       position = "top";
       height = 24;
-      modules-left = ["hyprland/workspaces"];
+      modules-left = ["hyprland/workspaces" "hyprland/window" "mpris"];
       modules-center = [];
       modules-right = ["tray" "idle_inhibitor" "custom/notification" "wireplumber" "battery" "clock"];
       "hyprland/window" = {
@@ -15,8 +15,8 @@
       "idle_inhibitor" = {
         format = "{icon}";
         format-icons = {
-          activated = "󰒳  ";
-          deactivated = "󰒲  ";
+          activated = "󰒳 ";
+          deactivated = "󰒲 ";
         };
       };
       "clock" = {
@@ -53,7 +53,7 @@
       };
       "custom/notification" = {
         tooltip = false;
-        format = "{icon} {}";
+        format = "{icon} {0}";
         format-icons = {
           notification = "󱥁 ";
           none = "󰍡 ";
@@ -95,67 +95,185 @@
           /* font-family: Helvetica Neue Light; */
       }
 
-      workspaces {
-          padding: 0 5px;
-      }
+window#waybar.hidden {
+  opacity: 0.2;
+}
+
+#waybar.empty #window {
+  background-color: transparent;
+}
 
 
-      #workspaces button {
-          padding: 0 5px;
-          color: #808080;
-      }
+#window {
+  margin: 2;
+  padding-left: 8;
+  padding-right: 8;
+  background-color: rgba(0,0,0,0.3);
+  font-size:14px;
+  font-weight: bold;
+}
 
-      #workspaces button.active {
-          color: #FFFFFF;
-      }
+button {
+  /* Use box-shadow instead of border so the text isn't offset */
+  box-shadow: inset 0 -3px transparent;
+  /* Avoid rounded borders under each button name */
+  border: none;
+  border-radius: 0;
+}
 
-      #mode {
-          background: #64727D;
-          border-bottom: 3px solid white;
-      }
+/* https://github.com/Alexays/Waybar/wiki/FAQ#the-workspace-buttons-have-a-strange-hover-effect */
+button:hover {
+  background: inherit;
+  border-top: 2px solid #c9545d;
+}
 
-      #clock, #battery, #cpu, #memory, #network, #wireplumber, #tray, #custom-weather, #custom-notification #idle_inhibitor {
-          padding: 0 5px;
-          margin: 0 2px;
-      }
+#workspaces button {
+  padding: 0 4px;
+  color: #FFFFFF; 
+}
 
-      #clock {
-      }
+#workspaces button:hover {
+}
 
-      #battery {
-      }
+#workspaces button.focused {
+  /*    box-shadow: inset 0 -2px #c9545d; */
+  background-color: rgba(0,0,0,0.3);
+  color:#c9545d;
+  border-top: 2px solid #c9545d;
+}
 
-      #battery icon {
-          color: red;
-      }
+#workspaces button.active {
+  /*    box-shadow: inset 0 -2px #c9545d; */
+  background-color: rgba(0,0,0,0.3);
+  color:#ffffff;
+  border-top: 1px solid #c9545d;
+}
 
-      #battery.charging {
-      }
+#workspaces button.urgent {
+  background-color: #eb4d4b;
+}
 
-      @keyframes blink {
-          to {
-             /* background-color: red; */
-              color: black;
-          }
-      }
+#mode {
+  background-color: #64727D;
+  border-bottom: 3px solid #ffffff;
+}
 
-      #battery.critical:not(.charging) {
-          background-color: red;
-          animation-name: blink;
-          animation-duration: 0.5s;
-          animation-timing-function: linear;
-          animation-iteration-count: infinite;
-          animation-direction: alternate;
-      }
+#clock,
+#battery,
+#cpu,
+#memory,
+#disk,
+#temperature,
+#backlight,
+#network,
+#pulseaudio,
+#wireplumber,
+#custom-media,
+#tray,
+#mode,
+#idle_inhibitor,
+#scratchpad,
+#custom-notification,
+#mpris {
+  margin: 2px;
+  padding-left: 4px;
+  padding-right: 4px;
+  background-color: rgba(0,0,0,0.3);
+  color: #ffffff;
+}
 
-      #battery.warning:not(.charging) {
-          background-color: yellow;
-          animation-name: blink;
-          animation-duration: 0.5s;
-          animation-timing-function: linear;
-          animation-iteration-count: infinite;
-          animation-direction: alternate;
-      }
+/* If workspaces is the leftmost module, omit left margin */
+.modules-left > widget:first-child > #workspaces {
+  margin-left: 0;
+}
+
+/* If workspaces is the rightmost module, omit right margin */
+.modules-right > widget:last-child > #workspaces {
+  margin-right: 0;
+}
+
+#clock {
+  font-size:14px;
+  font-weight: bold;
+}
+
+#battery icon {
+  color: red;
+}
+
+#battery.charging, #battery.plugged {
+  color: #ffffff;
+  background-color: #26A65B;
+}
+
+@keyframes blink {
+  to {
+    background-color: #ffffff;
+    color: #000000;
+  }
+}
+
+#battery.warning:not(.charging) {
+  background-color: #f53c3c;
+  color: #ffffff;
+  animation-name: blink;
+  animation-duration: 0.5s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+}
+
+#battery.critical:not(.charging) {
+  background-color: #f53c3c;
+  color: #ffffff;
+  animation-name: blink;
+  animation-duration: 0.5s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+}
+
+label:focus {
+  background-color: #000000;
+}
+
+#network.disconnected {
+  background-color: #f53c3c;
+}
+
+#temperature.critical {
+  background-color: #eb4d4b;
+}
+
+#idle_inhibitor.activated {
+  background-color: #ecf0f1;
+  color: #2d3436;
+}
+
+#tray > .passive {
+  -gtk-icon-effect: dim;
+}
+
+#tray > .needs-attention {
+  -gtk-icon-effect: highlight;
+  background-color: #eb4d4b;
+}
+
+/*
+
+window#waybar.solo {
+background-color: #FFFFFF;
+}
+
+window#waybar.termite {
+background-color: #3F3F3F;
+}
+
+window#waybar.chromium {
+background-color: #000000;
+border: none;
+}
+ */
     '';
   };
 }
