8db590fbac4fa335caa97785d91e29923a29a10b
spacefault <74156492+spacefault@users.noreply.github.com>
2025-12-02 14:51:29 -0700

    change emacs org font conf

--- home/terminal/emacs.nix
+++ home/terminal/emacs.nix
@@ -11,6 +11,7 @@
       epkgs.org-roam-ui
     ];
     extraConfig = ''
+; init
             (tool-bar-mode -1)
             (scroll-bar-mode -1)
             (setq org-startup-indented t)
@@ -19,16 +20,31 @@
             (require 'evil)
             ;(evil-mode 1)
             (display-battery-mode)
-            ;(setq catppuccin-flavor 'mocha)
-            ;(load-theme 'catppuccin :no-confirm)
+            (setq catppuccin-flavor 'mocha)
+            (load-theme 'catppuccin :no-confirm)
           (require 'nix-mode)
       (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode))
-      (require 'org-roam)
+
+; org conf
+
+(require 'org-roam)
       (setq org-roam-directory (expand-file-name "~/Documents/School/Notebox"))
       (org-roam-db-autosync-mode)
       (define-key global-map (kbd "C-c n l") 'org-roam-buffer-toggle)
       (define-key global-map (kbd "C-c n f") 'org-roam-node-find)
       (define-key global-map (kbd "C-c n i") 'org-roam-node-insert)
+(dolist (face '((org-level-1 . 1.35)
+                (org-level-2 . 1.3)
+                (org-level-3 . 1.2)
+                (org-level-4 . 1.1)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.1)
+                (org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font "Monaspace Radon" :weight 'bold :height (cdr face)))
+(add-hook 'org-mode-hook 'variable-pitch-mode)
+(custom-set-faces
+ '(variable-pitch ((t (:family "Monaspace Radon")))))
     '';
   };
 }
