0b2df24500db1a5dd39c914bf02ff2f77e9d04e2
spacefault <74156492+spacefault@users.noreply.github.com>
2025-12-09 21:05:15 -0700

    update ghostty init

--- home/terminal/ghostty.nix
+++ home/terminal/ghostty.nix
@@ -1,16 +1,4 @@
-{pkgs, ...}: let
-  ghosttyTmuxScript = pkgs.writeShellScript "ghostty-tmux.sh" ''
-    SESSION_NAME="ghostty"
-
-    if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-      WINDOW_ID=$(tmux new-window -t "$SESSION_NAME" -P -F "#{window_id}")
-      tmux attach-session -t "$SESSION_NAME" \; select-window -t "$WINDOW_ID"
-    else
-      tmux new-session -s "$SESSION_NAME"
-    fi
-
-  '';
-in {
+{pkgs, ...}:{
   programs.ghostty = {
     enable = true;
     package = null;
@@ -22,7 +10,6 @@ in {
       macos-option-as-alt = "true";
       theme = "Adventure";
 
-      command = "${pkgs.bash}/bin/bash -c '${ghosttyTmuxScript}'";
     };
   };
 }
